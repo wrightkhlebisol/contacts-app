41700c4150eabc1ba66a9e71929d5854c982d274
caleb ogundiya <caleb.ogundiya@engineering.digital.dwp.gov.uk>
2024-06-24 08:40:07 +0100

    refactor routes into sperate module

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,25 +1,53 @@
-import { createBrowserRouter } from "react-router-dom";
-import { getContact, getContacts, createContact } from "./contacts";
+import { createBrowserRouter, redirect } from "react-router-dom";
+import { getContact, getContacts, createContact, updateContact, deleteContact } from "./contacts";
 
 import Root from "./routes/root";
 import ErrorPage from "./error-page";
 import Contact from "./routes/contact";
 import EditContact from "./routes/edit";
+import Index from "./routes/index";
 
 
-export async function rootAction() {
+export async function createAction() {
   const contact = await createContact();
-  return { contact };
+  return redirect(`/contacts/${contact.id}/edit`);
 }
 
-export async function rootLoader() {
-  const contacts = await getContacts();
-  return { contacts };
+export async function rootLoader({request}) {
+  const url = new URL(request.url);
+  const q = url.searchParams.get('q');
+  const contacts = await getContacts(q);
+  return { contacts, q };
 }
 
-export async function loader({ params }) {
+export async function contactLoader({ params }) {
   const contact = await getContact(params.contactId);
+  if (!contact) {
+    throw new Response("", {
+      status: 404,
+      statusText: "Not Found",
+    })
+  }
   return {contact};
+}
+
+export async function editAction({ request, params }) {
+  const formData = await request.formData();
+  const updates = Object.fromEntries(formData);
+  await updateContact(params.contactId, updates);
+  return redirect(`/contacts/${params.contactId}`);
+}
+
+export async function destroyAction({ params }) {
+  await deleteContact(params.contactId);
+  return redirect(`/`);
+}
+
+export async function favAction({ request, params }) {
+  const formData = await request.formData();
+  return updateContact(params.contactId, {
+    favorite: formData.get("favorite") === "true",
+  });
 }
 
 const router = createBrowserRouter([
@@ -28,19 +56,32 @@ const router = createBrowserRouter([
     element: <Root />,
     errorElement: <ErrorPage />,
     loader: rootLoader,
-    action: rootAction,
+    action: createAction,
     children: [
       {
-        path: "contacts/:contactId",
-        element: <Contact />,
-        loader,
+        errorElement: <ErrorPage />,
+        children: [
+          { index: true, element: <Index /> },
+          {
+            path: "contacts/:contactId",
+            element: <Contact />,
+            loader: contactLoader,
+            action: favAction,
+          },
+          {
+            path: "contacts/:contactId/edit",
+            element: <EditContact />,
+            loader: contactLoader,
+            action: editAction,
+          },
+          {
+            path: "contacts/:contactId/destroy",
+            action: destroyAction,
+            errorElement: <div>Oops! There was an error.</div>
+          },
+        ]
       },
-      {
-        path: "contacts/:contactId/edit",
-        element: <EditContact />,
-        loader,
-      },
-    ],
+    ]
   },
 ]);
 
